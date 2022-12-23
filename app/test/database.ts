330648d1c7aaf5264b88447ad4c504afa248a97b
AlexKMarshall <alexmarshall@qogita.com>
2022-12-23 22:41:47 +0000

    test: create database util functions

--- app/test/database.ts
+++ app/test/database.ts
@@ -2,6 +2,23 @@ import { PrismaClient } from '@prisma/client'
 import { db } from '~/db.server'
 import { getNewSeedData } from './factories'
 import defaultSeedData from './seed-data.json'
+import { randomUUID } from 'node:crypto'
+import { execSync } from 'node:child_process'
+import url from 'node:url'
+import path from 'node:path'
+import fsPromises from 'node:fs/promises'
+
+const DATABASE_URL_FORMAT = 'file:./test/{{uuid}}.db'
+
+export function generateDatabaseUrl() {
+  return DATABASE_URL_FORMAT.replace('{{uuid}}', randomUUID())
+}
+
+export function migrateDatabase(url: string) {
+  return execSync(`pnpm turbo db:push -- --accept-data-loss`, {
+    env: { ...process.env, NODE_ENV: 'test', DATABASE_URL: url },
+  })
+}
 
 export function getPrismaClient(url?: string) {
   if (!url) return db
@@ -42,4 +59,26 @@ export async function seedDatabase(
   await Promise.all(
     subtasks.map((subtask) => db.subtask.create({ data: subtask }))
   )
+}
+
+export async function truncateDatabase(db = getPrismaClient()) {
+  const tables: { name: string }[] =
+    await db.$queryRaw`SELECT name FROM sqlite_schema WHERE type = 'table'`
+
+  await Promise.all(
+    tables.map(({ name }) => db.$executeRawUnsafe(`DELETE FROM ${name}`))
+  )
+}
+
+export async function prepareDatabase() {
+  const databaseUrl = generateDatabaseUrl()
+  await migrateDatabase(databaseUrl)
+  const client = getPrismaClient(databaseUrl)
+  return { databaseUrl, client }
+}
+
+export async function removeDatabase(databaseUrl: string) {
+  const relativePath = url.fileURLToPath(databaseUrl)
+  const fullPath = path.join(__dirname, '../../prisma', relativePath)
+  return fsPromises.unlink(fullPath)
 }
