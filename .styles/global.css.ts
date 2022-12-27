04b1bf11d2f66e760c08ad4bc5993d651cdabb68
AlexKMarshall <alexmarshall@qogita.com>
2022-12-27 15:44:29 +0000

    style: css reset

--- .styles/global.css.ts
+++ .styles/global.css.ts
@@ -1,12 +1,6 @@
 import { globalStyle } from '@vanilla-extract/css'
 
-// globalStyle('body', {
-//   '@media': {
-//     '(prefers-color-scheme: dark)': {
-//       backgroundColor: '#111',
-//     },
-//   },
-// })
+// CSS Reset
 
 globalStyle('*, *::before, *::after', {
   boxSizing: 'border-box',
